6786f8d6359a789e00093b2a82538c7145616b3c
ccstorebp18-cyber <ccstore.bp18@gmail.com>
2026-07-17 15:53:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+// TIMER
+let seconds = 0;
 
+setInterval(() => {
+  seconds++;
+
+  let min = Math.floor(seconds / 60);
+  let sec = seconds % 60;
+
+  document.getElementById("timer").innerText =
+    String(min).padStart(2, "0") + ":" +
+    String(sec).padStart(2, "0");
+
+}, 1000);
+
+
+// BUKA KERTAS
+function openPaper() {
+  document.getElementById("paper").style.display = "block";
+}
+
+
+// TUTUP KERTAS
+function closePaper() {
+  document.getElementById("paper").style.display = "none";
+}
+
+
+// CANVAS CORETAN
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 60;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+
+let drawing = false;
+
+
+canvas.addEventListener("pointerdown", (e) => {
+  drawing = true;
+
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+
+canvas.addEventListener("pointermove", (e) => {
+
+  if (!drawing) return;
+
+  ctx.lineWidth = 3;
+  ctx.lineCap = "round";
+
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+});
+
+
+canvas.addEventListener("pointerup", () => {
+  drawing = false;
+});
